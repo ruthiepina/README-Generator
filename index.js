e1ea7e9128de12f3db36305f49c3e25afc5f61d4
RMP <ruthiepina@gmail.com>
2023-12-13 00:13:06 -0700

    Included inquirer package. Created initial array of questions. Added inquirer prompt.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+   {
+      type: "input",
+      name: "projectName",
+      message: "What is your project name?",
+   },
+];
+
+inquirer.prompt(questions).then((response) => {
+   console.log("file: index.js:16 ~ response:", response);
+});
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
