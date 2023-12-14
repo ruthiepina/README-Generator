9555b95b20e738eb4badca314549c2c328fa666c
RMP <ruthiepina@gmail.com>
2023-12-13 21:30:54 -0700

    Added fs module, created writeToFile function, created readme file.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -14,8 +15,21 @@ inquirer.prompt(questions).then((response) => {
    console.log("file: index.js:16 ~ response:", response);
 });
 
+let readmeData = `HI RUTHIE`;
+
+writeToFile("./README.md", readmeData);
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+function writeToFile(fileName, data) {
+   fs.writeFile(fileName, data, "utf8", (err) => {
+      if (err) {
+         console.log("Error writing the file.", err);
+      } else {
+         console.log("File has been written.");
+      }
+   });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
