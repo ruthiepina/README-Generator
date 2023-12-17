01907c48de6572ff77732543ec0d0b297e6a6c55
RMP <ruthiepina@gmail.com>
2023-12-16 17:20:02 -0700

    Refactored generateMarkdown.

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 // TODO: Include packages needed for this application
-// import inquirer from "inquirer";
 import fs from "fs";
 import fetch from "node-fetch";
-import { createBadge } from "./utils/createBadge.js";
 import { inquirerPrompts } from "./utils/inquirerPrompts.js";
-
-
+import { generateMarkdown } from "./utils/generateMarkdown.js";
 const headers = {
    Accept: "application/vnd.github+json",
    "X-GitHub-Api-Version": "2022-11-28",
@@ -14,29 +11,11 @@ const gitHubAPI = "https://api.github.com/licenses";
 
 const main = async () => {
    const { answers, licenseTypes } = await inquirerPrompts(gitHubAPI, headers);
-   let myLicense = licenseTypes.filter((element) => element.name == answers.license);
 
-   const response = await fetch(gitHubAPI + "/" + myLicense[0].key, { headers });
-   const data = await response.json();
-
-   const svg = createBadge(myLicense[0].key);
-
-   let readme = `# ${answers.projectName} ${svg}
-## Project Description
-${answers.description}
-## Installation
-${answers.install}
-## Usage
-${answers.usage}
-## Contributions
-${answers.contributions}
-## Tests
-${answers.tests}
-## License
-${answers.license}
-${data.description}
-`;
-   writeToFile("./README.md", readme);
+   const fileData = await generateMarkdown(gitHubAPI, headers, answers, licenseTypes);
+   
+   const fileName = "./README.md";
+   writeToFile(fileName, fileData);
 };
 main();
 
